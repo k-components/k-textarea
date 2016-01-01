97c036cd89e5fdcdede79612a7f7a0ad00dd98b5
Ilkka Huotari <ilkkah@gmail.com>
2016-01-01 17:27:46 +0200

    focus & blur events

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ DerbyTextarea.prototype.init = function(model) {
 
 DerbyTextarea.prototype.create = function(model) {
 	var self = this;
-	
+
 	this.expandingArea.className += " active";
 
 	model.on('change', model.at('autofocus'), function(val) {
 		if (val) {
 			self.input.focus();
 		}
-	});	
+	});
 };
 
 DerbyTextarea.prototype.emitKeydown = function(ev, el) {
 	this.emit("keydown", ev, el);
+};
+
+DerbyTextarea.prototype.blur = function(ev, el) {
+	this.emit("blur", ev, el);
+};
+
+DerbyTextarea.prototype.focus = function(ev, el) {
+	this.emit("focus", ev, el);
 };
 
